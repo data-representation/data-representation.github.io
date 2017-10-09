16e60916ee4ca96e167f33fdf67b16fdfec43ad1
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2017-10-09 09:23:47 +0100

    Create webapps.go

---
+++ problems/webapps.go
@@ -0,0 +1,23 @@
+# Problem set: Web applications
+This problem set is for you to learn the fundamentals of [creating a web application in Go](https://golang.org/doc/articles/wiki/).
+Create a single Git repository as your submission, complete with README and gitignore files.
+*NB:* after completing each exercise commit your code - there should be at least one commit per exercise.
+You be will required to submit a URL for the repository and the use of [GitHub](http://www.github.com) is recommended for this purpose.
+All code should be fully commented, and the README should explain how to clone your repository and run the code.
+
+
+## 1. Hello, world!
+
+Create a web application in Go that responds with the text "Hello, world!".
+This should be the response (body) no matter what the request is.
+Explain in your README how to examine the response, including the headers, using curl.
+
+
+## 2. Current time
+
+Create a new route in your application at "/time".
+It should respond with the current date and time.
+
+
+## 3. 
+
